dd33e6b4a66e25c1505d071580bbe063c01ee713
sayaka-tamura <arccycle@gmail.com>
2019-04-23 01:45:48 +0900

    Added Test Coverage Part

--- src/test/java/com/course/practicaljava/PracticalJavaApplicationTests.java
+++ src/test/java/com/course/practicaljava/PracticalJavaApplicationTests.java
@@ -1,16 +1,13 @@
 package com.course.practicaljava;
 
-import org.junit.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+// Run all our test cases using JUnitPlatform library
+@RunWith(JUnitPlatform.class)
+// Test a package of "com.course.practicaljava"
+@SelectPackages("com.course.practicaljava")
 public class PracticalJavaApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
