57109bf69f35d413faf048979eb763fe33ae6c3f
sayaka-tamura <arccycle@gmail.com>
2019-04-24 00:34:38 +0900

    Added dependency for using kibana

--- src/test/java/com/course/practicaljava/PracticalJavaApplicationTests.java
+++ src/test/java/com/course/practicaljava/PracticalJavaApplicationTests.java
@@ -1,13 +1,13 @@
 package com.course.practicaljava;
-
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.runner.RunWith;
-
-// Run all our test cases using JUnitPlatform library
-@RunWith(JUnitPlatform.class)
-// Test a package of "com.course.practicaljava"
-@SelectPackages("com.course.practicaljava")
-public class PracticalJavaApplicationTests {
-
-}
+/**
+ * import org.junit.platform.runner.JUnitPlatform; import
+ * org.junit.platform.suite.api.SelectPackages; import org.junit.runner.RunWith;
+ * 
+ * // Run all our test cases using JUnitPlatform library
+ * 
+ * @RunWith(JUnitPlatform.class) // Test a package of
+ *                               "com.course.practicaljava" @SelectPackages("com.course.practicaljava")
+ *                               public class PracticalJavaApplicationTests {
+ * 
+ *                               }
+ */
